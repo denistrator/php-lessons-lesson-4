15d0a0defbcc053048cf6158fbeb45ec0a33a1d5
denistrator <denistrator@yandex.ua>
2018-08-05 02:19:14 +0300

    implement base puzzle functionality

---
+++ PHPPuzzle/Puzzle.php
@@ -0,0 +1,117 @@
+<?php
+
+class Puzzle
+{
+    const INPUT_UP = 119;
+    const INPUT_DOWN = 115;
+    const INPUT_LEFT = 97;
+    const INPUT_RIGHT = 100;
+
+    const ALLOWED_INPUT_CHARACTERS = [
+        self::INPUT_UP,
+        self::INPUT_DOWN,
+        self::INPUT_LEFT,
+        self::INPUT_RIGHT
+    ];
+
+    const SCREEN_SIZE_Y_MIN = 1;
+    const SCREEN_SIZE_Y_MAX = 4;
+    const SCREEN_SIZE_X_MIN = 1;
+    const SCREEN_SIZE_X_MAX = 4;
+
+    /* default pointer position Y */
+    private $pointerPosY = 2;
+
+    /* default pointer position X */
+    private $pointerPosX = 3;
+
+    /* default game screen */
+    private $screen = [
+        ['01', '02', '03', '04'],
+        ['05', '06', '  ', '07'],
+        ['09', '10', '11', '08'],
+        ['13', '14', '15', '12']
+    ];
+
+    private $inputCharacter;
+
+    public function updateBoard($inputCharacter)
+    {
+        if (in_array($inputCharacter, self::ALLOWED_INPUT_CHARACTERS)) {
+            $oldPointerPosY = $this->pointerPosY;
+            $oldPointerPosX = $this->pointerPosX;
+
+            $this->movePointer($inputCharacter);
+
+            $oldPointerCharacter = $this->screen[$oldPointerPosY - 1][$oldPointerPosX - 1];
+            $newPointerCharacter = $this->screen[$this->pointerPosY - 1][$this->pointerPosX - 1];
+
+            $this->screen[$oldPointerPosY - 1][$oldPointerPosX - 1] = $newPointerCharacter;
+            $this->screen[$this->pointerPosY - 1][$this->pointerPosX - 1] = $oldPointerCharacter;
+        }
+    }
+
+    public function run()
+    {
+        system('stty cbreak -echo');
+
+        $inputStream = fopen('php://stdin', 'r');
+
+        while (1) {
+            $this->geuUserInput($inputStream);
+
+            $this->updateBoard($this->inputCharacter);
+
+            $this->drawScreen($this->screen);
+        }
+    }
+
+    public function geuUserInput($input)
+    {
+        $this->inputCharacter = ord(fgetc($input));
+    }
+
+    public function drawScreen($screen)
+    {
+        echo str_repeat(PHP_EOL, 13);
+
+        foreach ($screen as $screenRow) {
+            foreach ($screenRow as $screenSymbol) {
+                echo $screenSymbol . '  ';
+            }
+
+            echo PHP_EOL;
+            echo PHP_EOL;
+        }
+
+        echo str_repeat(PHP_EOL, 10);
+    }
+
+    public function movePointer($direction)
+    {
+
+        if ($direction === self::INPUT_UP) {
+            if ($this->pointerPosY > self::SCREEN_SIZE_X_MIN) {
+                $this->pointerPosY -= 1;
+            }
+        }
+
+        if ($direction === self::INPUT_DOWN) {
+            if ($this->pointerPosY < self::SCREEN_SIZE_X_MAX) {
+                $this->pointerPosY += 1;
+            }
+        }
+
+        if ($direction === self::INPUT_LEFT) {
+            if ($this->pointerPosX > self::SCREEN_SIZE_X_MIN) {
+                $this->pointerPosX -= 1;
+            }
+        }
+
+        if ($direction === self::INPUT_RIGHT) {
+            if ($this->pointerPosX < self::SCREEN_SIZE_X_MAX) {
+                $this->pointerPosX += 1;
+            }
+        }
+    }
+}
---
+++ PHPPuzzle/play.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once 'Puzzle.php';
+
+$game = new Puzzle();
+$game->run();
